8a26c3560088d8252cde7ef732d93e6b19548a81
dteska <danielle.teska@gmail.com>
2017-01-21 21:54:58 -0800

    Add files via upload
    Ok, this time with the changes.

--- digitize/sketch.js
+++ digitize/sketch.js
@@ -73,4 +73,61 @@ function draw() {
   fill(237, 139, 46); // setting fill for orange rectangle same as lines
   rect(123, 187, 18, 9); // drawing 18x9 rectangle at 123,187
   
+  // And now to the fun stuff – two complex shapes. Dark, small one first...
+  noStroke(); //removing stroke
+  beginShape(); //telling it there will be a shape
+  fill(75, 32, 47); //pretty color. not really, mostly black.
+  vertex(118, 107); //telling the shape where to start
+  bezierVertex(118, 107, 137, 205, 118, 107); //anchor pt 1
+  bezierVertex(112, 134, 120, 144, 120, 144); //moving down 
+  bezierVertex(107, 203, 149, 177, 107, 203); //right edge
+  bezierVertex(93, 197, 106, 189, 106, 189); //curving to the left
+  bezierVertex(96, 182, 98, 196, 96, 182); // bump 1
+  bezierVertex(124, 179, 111, 153, 111, 153); //bump 2
+  bezierVertex(118, 107, 118, 107, 118, 107); //back to the start
+  endShape();
+  
+  // ...and the orange twisty complicated one.
+  noStroke(); //setting no stroke
+  beginShape(); //it's on.
+  fill(237, 139, 46); //setting the color for the shape
+  vertex(53, 16); //giving the shape a starting point
+  bezierVertex(46, 17, 53, 16, 53, 16); //starting at the upper left
+  bezierVertex(43, 42, 52, 36, 52, 36); //second anchor point (going counter clockwise)
+  bezierVertex(49, 60, 52, 56, 52, 56); //third anchor point along left
+  bezierVertex(50, 88, 52, 93, 50, 88); //fourth anchor point along left
+  bezierVertex(58, 106, 55, 102, 56, 92); //fifth anchor point, ducks under
+  bezierVertex(58, 106, 56, 114, 58, 106); //sixth directly under
+  bezierVertex(57, 137, 51, 141, 57, 137); //seventh directly under
+  bezierVertex(57, 137, 49, 151, 57, 153); //eight – a little bounce
+  bezierVertex(57, 137, 62, 202, 54, 185); //ninth – getting closer
+  bezierVertex(59, 239, 59, 239, 59, 239); //tenth – bottom left edge
+  bezierVertex(63, 224, 63, 224, 63, 224); //eleventh – and up
+  bezierVertex(65, 234, 75, 233, 65, 234); //twelfth – and back down
+  bezierVertex(67, 218, 72, 222, 72, 222); //thirteenth -bottom right edge
+  bezierVertex(71, 207, 79, 205, 71, 207); //fourteenth 
+  bezierVertex(68, 193, 79, 205, 75, 197); //fifteenth 
+  bezierVertex(59, 166, 65, 172, 65, 172); //Sixteenth  
+  bezierVertex(68, 122, 68, 122, 68, 128); //seventeenth 
+  bezierVertex(68, 106, 78, 105, 68, 106); //eighteenth 
+  bezierVertex(82, 114, 82, 114, 82, 114); //ninteenth 
+  bezierVertex(85, 106, 86, 93, 85, 106); //twentieth 
+  bezierVertex(72, 96, 72, 96, 72, 96); //21st
+  bezierVertex(61, 80, 61, 80, 61, 80); //22nd
+  bezierVertex(47, 40, 59, 56, 59, 56); //23nd 
+  bezierVertex(64, 43, 70, 50, 64, 43); //24nd 
+  bezierVertex(81, 65, 96, 75, 81, 65); //25nd 
+  bezierVertex(91, 58, 87, 45, 91, 58); //26nd 
+  bezierVertex(112, 48, 112, 48, 112, 48); //27nd 
+  bezierVertex(102, 23, 117, 37, 117, 37); //28nd 
+  bezierVertex(131, 32, 150, 34, 131, 32); //29nd 
+  bezierVertex(154, 11, 170, 26, 154, 11); //30nd 
+  bezierVertex(160, 14, 182, 19, 182, 19); //31nd rightmost edge
+  bezierVertex(148, 30, 154, 6, 154, 6); //32nd 
+  bezierVertex(121, 24, 121, 24, 121, 24); //33nd 
+  bezierVertex(113, 14, 113, 14, 113, 14); //34th 
+  bezierVertex(101, 8, 102, 17, 102, 17); //35th 
+  bezierVertex(78, 11, 102, 17, 102, 17); //36th 
+  bezierVertex(23, 16, 53, 16, 53, 16); //giving the shape an ending point
+  endShape(); //and done.
 }
